93bc08a8932583bb748aaf77c76c71c33d790e92
yussufnarcos <narcosyussuf@gmail.com>
2025-02-27 15:19:05 +0300

    add phase-1-javascript-variables-lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+// 1. companyName should be a constant and set to 'Scuber'
+const companyName = "Scuber";
+
+// 2. mostProfitableNeighborhood should be defined using let and set to 'Chelsea'
+let mostProfitableNeighborhood = "Chelsea";
+
+// 3. companyCeo should be defined using let and set to 'Susan Smith'
+let companyCeo = "Susan Smith";
